39e28d8303435c835bb4d94668f58bd88a6cdb7c
junaidjahan <70884217+junaidjahan@users.noreply.github.com>
2024-10-25 16:28:10 +0500

    chore: add eslint and prettier config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   build: {
-    transpile: ['vuetify'],
+    transpile: ["vuetify"],
   },
   modules: [
+    "@nuxt/eslint",
     (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        config.plugins?.push(vuetify({ autoImport: true }));
+      });
     },
-    //...
   ],
   vite: {
     vue: {
@@ -23,4 +22,4 @@ export default defineNuxtConfig({
       },
     },
   },
-})
+});
